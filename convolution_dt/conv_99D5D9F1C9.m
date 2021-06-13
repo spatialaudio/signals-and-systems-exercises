eb082fe213dbacad0640dcf74724fdf1f8bc71a6
Frank Schultz <scf175@googlemail.com>
2021-06-13 13:36:46 +0200

    sig_sys_ex_08 related mods

--- convolution_dt/conv_99D5D9F1C9.m
+++ convolution_dt/conv_99D5D9F1C9.m
@@ -1,5 +1,25 @@
+% [Sascha Spors](https://orcid.org/0000-0001-7225-9992),
+% Professorship Signal Theory and Digital Signal Processing,
+% [Institute of Communications Engineering (INT)](https://www.int.uni-rostock.de/),
+% Faculty of Computer Science and Electrical Engineering (IEF),
+% [University of Rostock, Germany](https://www.uni-rostock.de/en/)
+% 
+% # Tutorial Signals and Systems (Signal- und Systemtheorie)
+% 
+% Summer Semester 2021 (Bachelor Course #24015)
+% 
+% - lecture: https://github.com/spatialaudio/signals-and-systems-lecture
+% - tutorial: https://github.com/spatialaudio/signals-and-systems-exercises
+% 
+% WIP...
+% The project is currently under heavy development while adding new material for the summer semester 2021
+% 
+% Feel free to contact lecturer [frank.schultz@uni-rostock.de](https://orcid.org/0000-0002-3010-0294)
+
+% Exercise 8.4
+
 clear all
-%close all
+close all
 clc
 
 M = 7;
@@ -37,6 +57,15 @@ disp('|M-N|+1')
 abs(M-N)+1
 max(y)-1
 
-
-
-
+% ## Copyright
+% 
+% This tutorial is provided as Open Educational Resource (OER), to be found at
+% https://github.com/spatialaudio/signals-and-systems-exercises
+% accompanying the OER lecture
+% https://github.com/spatialaudio/signals-and-systems-lecture.
+% Both are licensed under a) the Creative Commons Attribution 4.0 International
+% License for text and graphics and b) the MIT License for source code.
+% Please attribute material from the tutorial as *Frank Schultz,
+% Continuous- and Discrete-Time Signals and Systems - A Tutorial Featuring
+% Computational Examples, University of Rostock* with
+% ``main file, github URL, commit number and/or version tag, year``.
--- convolution_dt/convolution_discrete_FD58EEB1EC.m
+++ convolution_dt/convolution_discrete_FD58EEB1EC.m
@@ -1,3 +1,23 @@
+% [Sascha Spors](https://orcid.org/0000-0001-7225-9992),
+% Professorship Signal Theory and Digital Signal Processing,
+% [Institute of Communications Engineering (INT)](https://www.int.uni-rostock.de/),
+% Faculty of Computer Science and Electrical Engineering (IEF),
+% [University of Rostock, Germany](https://www.uni-rostock.de/en/)
+% 
+% # Tutorial Signals and Systems (Signal- und Systemtheorie)
+% 
+% Summer Semester 2021 (Bachelor Course #24015)
+% 
+% - lecture: https://github.com/spatialaudio/signals-and-systems-lecture
+% - tutorial: https://github.com/spatialaudio/signals-and-systems-exercises
+% 
+% WIP...
+% The project is currently under heavy development while adding new material for the summer semester 2021
+% 
+% Feel free to contact lecturer [frank.schultz@uni-rostock.de](https://orcid.org/0000-0002-3010-0294)
+
+% Exercise 8.2
+
 clear all
 close all
 clc
@@ -36,3 +56,16 @@ k = ky:ky+Ny-1
 stem(k,y)
 xlabel('k')
 ylabel('y[k]=x[k]*h[k]')
+
+% ## Copyright
+% 
+% This tutorial is provided as Open Educational Resource (OER), to be found at
+% https://github.com/spatialaudio/signals-and-systems-exercises
+% accompanying the OER lecture
+% https://github.com/spatialaudio/signals-and-systems-lecture.
+% Both are licensed under a) the Creative Commons Attribution 4.0 International
+% License for text and graphics and b) the MIT License for source code.
+% Please attribute material from the tutorial as *Frank Schultz,
+% Continuous- and Discrete-Time Signals and Systems - A Tutorial Featuring
+% Computational Examples, University of Rostock* with
+% ``main file, github URL, commit number and/or version tag, year``.
